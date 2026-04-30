379f963811374cd6a08328baee8c5d2d786d6e8c
mairame <mairameniang87@gmail.com>
2026-04-30 22:07:10 +0000

    Lab 2 : composants statiques LigneBus et ListeLignes

---
+++ src/Statistique.js
@@ -0,0 +1,12 @@
+import './Statistique.css';
+
+function Statistique() {
+  return (
+    <div className="statistique">
+      <span>10</span>
+      <span>Lignes</span>
+    </div>
+  );
+}
+
+export default Statistique;
